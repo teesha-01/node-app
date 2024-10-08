6bf931013283183ac341fb2c1905d8235205611c
teesha-01 <146992627+teesha-01@users.noreply.github.com>
2024-10-08 15:08:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World this is from test branch!')
+  response.send('Hello World this is from node-app branch!')
 })
 
 app.listen(app.get('port'), function() {
